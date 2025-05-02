951f29808b3d80787c736cbb681fbec925992a68
MrLightful <romatallinn@gmail.com>
2025-05-02 17:16:03 +0200

    Fix homepage layout

--- src/app/routes/home.tsx
+++ src/app/routes/home.tsx
@@ -3,18 +3,20 @@ import GithubStarButton from '@/features/github-star-button'
 
 export function HomePage() {
     return (
-        <div className="mx-32 my-32 m-auto text-center space-y-8">
-            <div className="space-y-3">
-                <BuiltWith />
-                <h1 className="text-3xl items-center">
-                    Welcome to Tauri Core template!
-                </h1>
-                <p>
-                    This template is a starting point for building Tauri apps
-                    with Vite, React, and Tailwind CSS.
-                </p>
+        <div className="flex h-screen">
+            <div className="m-auto text-center space-y-3">
+                <div className="space-y-3">
+                    <BuiltWith />
+                    <h1 className="text-3xl items-center">
+                        Welcome to Tauri Core template!
+                    </h1>
+                    <p>
+                        This template is a starting point for building Tauri
+                        apps with Vite, React, and Tailwind CSS.
+                    </p>
+                </div>
+                <GithubStarButton />
             </div>
-            <GithubStarButton />
         </div>
     )
 }

3b446b31ff1dde2ac7a21138d6c9e0ba6c6e6701
MrLightful <romatallinn@gmail.com>
2025-05-02 16:22:15 +0200

    Minor improvements

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,12 +1,14 @@
-import { ReactNode } from 'react'
+import { ReactNode, Suspense } from 'react'
 import AppErrorPage from '@/features/errors/app-error'
 import { ErrorBoundary } from 'react-error-boundary'
 import { TooltipProvider } from '@/components/ui/tooltip'
 
 export default function AppProvider({ children }: { children: ReactNode }) {
     return (
-        <ErrorBoundary FallbackComponent={AppErrorPage}>
-            <TooltipProvider>{children}</TooltipProvider>
-        </ErrorBoundary>
+        <Suspense fallback={<>Loading...</>}>
+            <ErrorBoundary FallbackComponent={AppErrorPage}>
+                <TooltipProvider>{children}</TooltipProvider>
+            </ErrorBoundary>
+        </Suspense>
     )
 }
